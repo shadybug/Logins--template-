28f0b015ed380608bf4c07136864185c9e935226
Cicada Carpenter <cicada.carpenter@gmail.com>
2022-12-14 12:56:38 -0700

    Created template code

--- app.py
+++ app.py
@@ -1,67 +1,84 @@
+## Template code for a login Flask project.
+## Two ##s is an explanation or label, and one # is pseudocode that should be replaced
+
+## Imports
+
+## Flask libraries (main library, display template pages, request data, redirect to a new page, and store session data)
 from flask import Flask, render_template, request, redirect, session
+## SQLlchemy library
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-## start SQL Alchemy things
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-app.secret_key = "Super Secret"
+## Start SQL Alchemy things
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db' ## Where we store the database
+app.secret_key = "Super Secret" ## Secret key for sessions
 
 db = SQLAlchemy(app)
 
+## Create the User table in the database
 class User(db.Model):
+    ## Column 1: user ID, as a number
     id = db.Column(db.Integer, primary_key=True)
+    ## Column 2: username, as a string
     username = db.Column(db.String(80), unique=True, nullable=False)
+    ## Column 3: password, as a string
     password = db.Column(db.String(120), unique=True, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
-## end SQL Alchemy things
+        return '<User %r>' % self.usernames
+
+## End SQL Alchemy things
+
+## Main Page
 
 @app.route("/")
 def index():
-    if "username" in session:
-        return render_template('home.html', user = session['username'])
-    else:
-        return render_template('login.html')
+    pass
+    # If user is logged in, render the homepage
+    # Otherwise, render the login page
+
+## Logout Page
+
+@app.route("/logout")
+def logout():
+    pass
+    # Log the user out
+
+## Login Page
 
 @app.route("/login", methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
+        # When we press the login button, get the username and password data
 
-        user = User.query.filter_by(username=username).first()
-
-        if user and (password == user.password):
-            session['username'] = username
-            return redirect("/")
-        else:
-            return render_template('login.html', error = "Invalid username or password.")
+        user = User.query.filter_by(username=username).first() ## Find the username and password in the database
+        
+        # Determine if the username and password are in the database
+            # If so, log them in!
+            # If not, display an error
     else:
         return render_template('login.html')
+
+## Signup Page
 
 @app.route("/signup", methods=['POST', 'GET'])
 def signup():
     if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-        confirm_password = request.form["confirm-password"]
+        # When we press the signup button, get the username and password data
+        # (Don't forget the confirm password field!)
 
-        if password != confirm_password:
-            return render_template('signup.html', error = "Passwords do not match!")
+        # If the passwords don't match, return an error
         
-        user = User.query.filter_by(username=username).first()
-        if user:
-            return render_template('signup.html', error = "Username already exists.")
-        
-        new_user = User(username=username, password=password)
-        db.session.add(new_user)
-        db.session.commit()
+        user = User.query.filter_by(username=username).first() ## Find the username and password in the database
 
-        return redirect("/")
+        # If the username already exists, return an error
+        # Otherwise, add the new user to the database and return to the homepage.
     else:
         return render_template('signup.html')
+
+## Setup
 
 if __name__ == "__main__":
   db.create_all()
